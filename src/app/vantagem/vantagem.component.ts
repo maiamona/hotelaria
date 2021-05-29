a2f5d8e6864b5502f371da4693a606e6a7c90b8c
maiamona.garcia <maiamona.garcia@jupiter.co.ao>
2021-05-29 07:11:22 +0100

    hotelaria

---
+++ src/app/vantagem/vantagem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vantagem',
+  templateUrl: './vantagem.component.html',
+  styleUrls: ['./vantagem.component.css']
+})
+export class VantagemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
